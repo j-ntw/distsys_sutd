e533a358b8f9e59ecc9f1e193f1f75e54c8edb5f
Joshua Ng <joshuantw@gmail.com>
2023-10-01 17:30:54 +0800

    adjust vector clock

--- hw1/hw1_3.go
+++ hw1/hw1_3.go
@@ -9,43 +9,63 @@ import (
 const (
 	numClients  = 10
 	timeDilator = 10
+	serverClock = 11
+	numEntities = numClients + 1
 )
 
 type Msg struct {
-	id   int
-	data int
+	id    int
+	data  int
+	clock [numEntities]int
 }
 
 func server(ch_arr [numClients]chan Msg, ch_server chan Msg) {
+	var clock = [numEntities]int{}
 	fmt.Println("start server")
 	for {
+		// increment own clock
+		clock[numClients] += 1 * serverClock
+
 		// check which channel needs attention
 		in_msg := <-ch_server
+		fmt.Printf("server recieve from c_%d: %d, clock_%d\n ", in_msg.id, in_msg.data, in_msg.clock)
+
+		// adjust clock
+		clock = adjustClock(numClients, clock, in_msg.clock)
+
 		// flip a coin to send or drop
-		fmt.Printf("server recieve from c_%d: %d\n", in_msg.id, in_msg.data)
 		if coinFlip() {
 			go broadcast(in_msg, ch_arr)
 		} else {
-			fmt.Printf("server drop: c_%d: %d\n", in_msg.id, in_msg.data)
+			fmt.Printf("server drop: c_%d: %d, clock_%d\n", in_msg.id, in_msg.data, in_msg.clock)
 		}
 
 	}
 }
 
 func client(ch_client chan Msg, client_id int, ch_server chan Msg) {
+	var clock = [numEntities]int{}
 	fmt.Printf("start c_%d\n", client_id)
 	for {
+		// increment own clock
+		clock[client_id] += 1 * client_id
+
 		// create message
-		out_msg := Msg{client_id, rand.Intn(10000)}
+		out_msg := Msg{client_id, rand.Intn(10000), clock}
+
 		// send on public channel
 		ch_server <- out_msg
+		fmt.Printf("c_%d send to server: %d, clock_%d\n", out_msg.id, out_msg.data, out_msg.clock)
 
-		fmt.Printf("c_%d send to server: %d\n", out_msg.id, out_msg.data)
-		sleepRand() // do i need to sleep for nonzero time
+		// sleep for nonzero time
+		sleepRand()
 		go func() {
 			// recieve on private channel
 			in_msg := <-ch_client
-			fmt.Printf("c_%d recieve from c_%d: %d\n", client_id, in_msg.id, in_msg.data)
+			fmt.Printf("c_%d recieve from c_%d: %d, clock_%d\n", client_id, in_msg.id, in_msg.data, in_msg.clock)
+
+			// adjust clock
+			clock = adjustClock(client_id, clock, in_msg.clock)
 		}()
 	}
 }
@@ -70,6 +90,19 @@ func broadcast(broadcast_msg Msg, ch_arr [numClients]chan Msg) {
 		}
 	}
 }
+
+func adjustClock(id int, clock [numEntities]int, msg_clock [numEntities]int) [numEntities]int {
+	if msg_clock[id] > clock[id] {
+		fmt.Printf("adjust clock: %v->%v", clock, msg_clock)
+		return msg_clock
+
+	} else {
+		fmt.Printf("adjust clock: %v->%v", clock, clock)
+		return clock
+	}
+
+}
+
 func main() {
 	var ch_arr [numClients]chan Msg
 	var ch_server chan Msg = make(chan Msg)
