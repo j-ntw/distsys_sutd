745db063334d403c7a95e811303f0ad2e917cc94
Joshua Ng <joshuantw@gmail.com>
2023-11-26 22:33:36 +0800

    add more deets

--- hw3/main.go
+++ hw3/main.go
@@ -1,9 +1,40 @@
 package main
 
+type AccessType int
+
+const (
+	ReadOnly AccessType = iota
+	WriteOnly
+	ReadWrite
+)
+
+type MessageType int
+
+const (
+	ReadRequest MessageType = iota
+	WriteRequest
+	ReadForward
+	WriteForward
+	SendPage
+	ReadConfirmation
+	WriteConfirmation
+	Invalidate
+	InvalidateConfirmation
+)
+
 type Msg struct {
 	// Define your Msg structure fields here
+	messageType MessageType
+	from        int
+	to          int
+	page        Page
+	accessType  AccessType //AccessType is for sendPage
 }
-type Node struct {
+type Page struct {
+	id   int // owner process id
+	data int
+}
+type Process struct {
 	ch     chan Msg
 	id     int
 	page   int
@@ -15,22 +46,32 @@ type CM struct {
 	page     int
 	copy_set Set
 	owner    int
+	pages    []Page
 }
 
+// CM maintains record for all pages
+// the owner and all its copies
+// TODO: does each process own multiple pages?
+// only one process can own a page at a time (no co owner, ownership can be passed around)
+// each page can have at most one owner. there can be page that is orphaned
+
 var (
-	numNodes = 3
-	// node_arr node_arr [numNodes]Node
-	// ch_arr [numNodes]chan Msg
+	numprocesss = 3
+	// process_arr process_arr [numprocesss]process
+	// ch_arr [numprocesss]chan Msg
 
 )
 
 func main() {
 
 	// single central manager (with one backup)
-	// a few other nodes/processes
+	// a few other processes
+	// P3 wants to read page x1 (page fault at P3)
+	// P3 sends read req to CM (X1. P1)
+	// CM checks page owner and sends read forward to owner P1, adds P3 in copy set
 
 	go CM.Run()
-	for n := range node_arr {
+	for n := range process_arr {
 		n.Run()
 	}
 }
